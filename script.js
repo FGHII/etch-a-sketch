58e676c2a2c07316c6ec57c0b1829bd6b132471f
FGHII <eric.j.klosterman@gmail.com>
2021-04-22 12:41:53 -0500

    added reset button

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const gridContainer = document.querySelector(".grid-container");
-let rowCount = 16;
-while (rowCount > 0) {
-  // create each row
-  const row = document.createElement('div');
-  row.classList.add('row');
-  gridContainer.appendChild(row);
-  rowCount--;
-  // create grid squares in the row
-  let squareCount = 16;
-  while (squareCount > 0) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener("mouseover", function(event) {
-      square.classList.add("color");
-    });
-    row.appendChild(square);
-    squareCount--;
+function generateGrid(rowCount, squareCount) {
+  const gridContainer = document.querySelector(".grid-container");
+  while (rowCount > 0) {
+    // create each row
+    const row = document.createElement('div');
+    row.classList.add('row');
+    gridContainer.appendChild(row);
+    squareBuildCount = squareCount;
+    // create grid squares in the row
+    while (squareBuildCount > 0) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.addEventListener("mouseover", function(event) {
+        square.classList.add("color");
+      });
+      row.appendChild(square);
+      squareBuildCount--;
+    }
+    rowCount--;
   }
 }
+//initialize grid up on site load
+let rowCount = 16;
+let squareCount = 16;
+generateGrid(rowCount, squareCount);
+
+//reset button functionality
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", function() {
+  document.querySelectorAll(".square.color").forEach(function(square) {
+    square.classList.remove("color");
+  });
+  newGridSize = prompt("Enter new grid size:");
+  const gridContainer = document.querySelector(".grid-container");
+  //remove all child nodes from grid container to erase the grid
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+  generateGrid(newGridSize, newGridSize);
+})
